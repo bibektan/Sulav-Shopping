64e92bab230f79df30ce9d0333d898ea892cf9a8
bibektan <bibektan@gmail.com>
2022-12-21 22:53:45 +0545

    Express Router setup

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workout')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get('/', (req, res)=>{
-    res.json({msg: "hello response"})
-})
+app.use('/api/workouts' ,workoutRoutes)
 
 // listening request
 app.listen(process.env.PORT, ()=>{
